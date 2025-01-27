6f55e6be8a2584301d4977f82d6dd28f09d0e3eb
chuy-zip <andreschuy03@gmail.com>
2025-01-26 20:48:00 -0600

    now uses 2 features

--- ej2_1.py
+++ ej2_1.py
@@ -1,5 +1,12 @@
+# Este código fue realizado con el apoyo de la siguiente explicación
+# https://towardsdatascience.com/logistic-regression-from-scratch-in-python-ec66603592e2
+# La implementación en la guias es un tanto mas compleja e incluye mas calculos
+# por lo que esta version esta simplificada pero siempre se cumplen con los conceptos escenciales
+# para una regresion logistica
+
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 def sigmoid(z):
     return 1.0/(1 + np.exp(-z))
@@ -9,22 +16,26 @@ df_phish = pd.read_csv("dataset_phishing.csv")
 print(df_phish.shape)
 print(df_phish.columns)
 #La columna status realmente debería de ser 0 o 1 para identyificar si es legitima o phisihing respectivamente
-print(df_phish["status"])
+
 
 # Convertir valores de 'status' a valores binarios
 df_phish['status'] = df_phish['status'].map({'legitimate': 0, 'phishing': 1})
-print(df_phish['status'].unique())  # Deberías ver [0, 1]
+print(df_phish['status'].unique())  #[0,1]
+print("New phishhhhhhhhhhhhhhhhhh")
+print(df_phish["status"])
 
 #remover las columnas no numericas para probar (tal vez reduzca las columasn luego)
-df = df_phish.drop(columns=["url", "status"])
+#df = df_phish.drop(columns=["url"])
 
-#df = df_phish[["length_url","https_token", "punycode"]]
+df = df_phish[["length_url","https_token"]]
 
 print(df.shape)
+
 
 # Ahora que tengo 87 columnas necesito 87 pesos w
 X = df.values
 Y = df_phish['status'].values
+m = df.shape[0] # la cantidad de registros en el dataframe de padnas
 W = np.zeros(df.shape[1])
 b = 0 # sesgo 0 de momento
 
@@ -32,10 +43,22 @@ b = 0 # sesgo 0 de momento
 learning_rate = 0.01  
 num_iterations = 1000
 
-z = np.dot(X, W) + b
+for iteration in range(num_iterations):
 
-print(z)
+    # calculo de  z = X * W + b
+    z = np.dot(X, W) + b
 
-# hipotesis/ predicciones
-y_hat = sigmoid(z)
-print(y_hat)
+    # hipotesis/ predicciones
+    y_hat = sigmoid(z)
+
+    # gradientes con la foprmula ya derivada
+    dw = (1/m) * np.dot(X.T, (y_hat - Y))  # Gradiente W
+    db = (1/m) * np.sum(y_hat - Y)  # Gradiente b
+
+    W -= learning_rate * dw
+    b -= learning_rate * db
+
+print("Pesos finales:", W)
+print("Sesgo final:", b)
+
+# Graficar
